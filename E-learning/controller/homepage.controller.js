683c5bd106f55f921e3e4dd56ce898af2770e9df
hieuvu99 <star.hieu.99@gmail.com>
2020-12-28 16:04:41 +0700

    Tạo bố cục màn hình home với carousel và slider list

---
+++ E-learning/controller/homepage.controller.js
@@ -0,0 +1,69 @@
+module.exports = {
+  getHighlightCourse:  (req, res) => {
+    // let courses = [
+    //   {
+    //     name: "Lập trình game cơ bản",
+    //     category: "Lập trình web",
+    //     teacher: "Khoa Trần",
+    //     rating: 5,
+    //     image: ""
+    //   },
+    //   {
+    //     name: "Lập trình di động nâng cao",
+    //     category: "Lập trình di động",
+    //     teacher: "Nhân Trần",
+    //     rating: 5,
+    //     image: ""
+    //   },
+    //   {
+    //     name: "Lập trình Android",
+    //     category: "Lập trình di động",
+    //     teacher: "Hiếu Vũ",
+    //     rating: 5,
+    //     image: ""
+    //   },
+    //   {
+    //     name: "Lập trình Website cơ bản",
+    //     category: "Lập trình web",
+    //     teacher: "Khoa Trần",
+    //     rating: 5,
+    //     image: ""
+    //   },
+    //   {
+    //     name: "Lập trình Website nâng cao",
+    //     category: "Lập trình web",
+    //     teacher: "Ngọc Nguyễn",
+    //     rating: 5,
+    //     image: ""
+    //   },
+    //   {
+    //     name: "Lập trình iOS",
+    //     category: "Lập trình di động",
+    //     teacher: "Lộc Linh",
+    //     rating: 5,
+    //     image: ""
+    //   },
+    //   {
+    //     name: "Lập trình game nâng cao",
+    //     category: "Lập trình web",
+    //     teacher: "Hoàng Đỗ",
+    //     rating: 5,
+    //     image: ""
+    //   }
+    // ];
+    // res.render("vwHomepage/home");
+  },
+
+  getMostViewedCourse:  (req, res) => {
+
+  },
+
+  getLatestCourse:  (req, res) => {
+
+  },
+
+  getMostRegisteredCourse: (req, res) => {
+
+  }
+  
+};
--- E-learning/middlewares/routes.mdw.js
+++ E-learning/middlewares/routes.mdw.js
@@ -1,6 +1,65 @@
 module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.render('home');
+  app.get("/", function (req, res) {
+    let courses = [
+      {
+        name: "Lập trình game cơ bản",
+        category: "Lập trình web",
+        teacher: "Khoa Trần",
+        rating: 5,
+        image: "",
+      },
+      {
+        name: "Lập trình di động nâng cao",
+        category: "Lập trình di động",
+        teacher: "Nhân Trần",
+        rating: 5,
+        image: "",
+      },
+      {
+        name: "Lập trình Android",
+        category: "Lập trình di động",
+        teacher: "Hiếu Vũ",
+        rating: 5,
+        image: "",
+      },
+      {
+        name: "Lập trình Website cơ bản",
+        category: "Lập trình web",
+        teacher: "Khoa Trần",
+        rating: 5,
+        image: "",
+      },
+      {
+        name: "Lập trình Website nâng cao",
+        category: "Lập trình web",
+        teacher: "Ngọc Nguyễn",
+        rating: 5,
+        image: "",
+      },
+      {
+        name: "Lập trình iOS",
+        category: "Lập trình di động",
+        teacher: "Lộc Linh",
+        rating: 5,
+        image: "",
+      },
+      {
+        name: "Lập trình game nâng cao",
+        category: "Lập trình web",
+        teacher: "Hoàng Đỗ",
+        rating: 5,
+        image: "",
+      },
+    ];
+    res.render("vwHomepage/home", {
+      courses: {
+        name: "Lập trình game nâng cao",
+        category: "Lập trình web",
+        teacher: "Hoàng Đỗ",
+        rating: 5,
+        image: "",
+      },
+    });
   });
-  app.use('/account', require('../routes/front/account.route'));
-}
+  app.use("/account", require("../routes/front/account.route"));
+};
