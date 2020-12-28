238f663e73fd505e1750343f6b3a8272a64104eb
hieuvu99 <star.hieu.99@gmail.com>
2020-12-28 23:35:05 +0700

    Render course list with mock data

--- E-learning/middlewares/routes.mdw.js
+++ E-learning/middlewares/routes.mdw.js
@@ -6,59 +6,32 @@ module.exports = function (app) {
         category: "Lập trình web",
         teacher: "Khoa Trần",
         rating: 5,
-        image: "",
+        image: "https://i.imgur.com/vHNHZCC.jpg",
       },
       {
         name: "Lập trình di động nâng cao",
         category: "Lập trình di động",
         teacher: "Nhân Trần",
         rating: 5,
-        image: "",
+        image: "https://i.imgur.com/vHNHZCC.jpg",
       },
       {
         name: "Lập trình Android",
         category: "Lập trình di động",
         teacher: "Hiếu Vũ",
         rating: 5,
-        image: "",
+        image: "https://i.imgur.com/vHNHZCC.jpg",
       },
       {
         name: "Lập trình Website cơ bản",
         category: "Lập trình web",
         teacher: "Khoa Trần",
         rating: 5,
-        image: "",
-      },
-      {
-        name: "Lập trình Website nâng cao",
-        category: "Lập trình web",
-        teacher: "Ngọc Nguyễn",
-        rating: 5,
-        image: "",
-      },
-      {
-        name: "Lập trình iOS",
-        category: "Lập trình di động",
-        teacher: "Lộc Linh",
-        rating: 5,
-        image: "",
-      },
-      {
-        name: "Lập trình game nâng cao",
-        category: "Lập trình web",
-        teacher: "Hoàng Đỗ",
-        rating: 5,
-        image: "",
+        image: "https://i.imgur.com/vHNHZCC.jpg",
       },
     ];
     res.render("vwHomepage/home", {
-      courses: {
-        name: "Lập trình game nâng cao",
-        category: "Lập trình web",
-        teacher: "Hoàng Đỗ",
-        rating: 5,
-        image: "",
-      },
+      courses: courses,
     });
   });
   app.use("/account", require("../routes/front/account.route"));
