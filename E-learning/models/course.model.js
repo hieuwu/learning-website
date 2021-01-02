4379a81cfa4257b8357f11fcf8a9155f3308af62
hieuvu99 <star.hieu.99@gmail.com>
2021-01-02 16:06:34 +0700

    Create router for header category - domain

--- E-learning/models/course.model.js
+++ E-learning/models/course.model.js
@@ -77,5 +77,40 @@ module.exports = {
         on course.IdCategory = category.Id
         where category.Id= ${catId}`);
         return rows[0].total;
-    }
+    },
+    async countByDomain(domainId) {
+        let rows = await db.load(`select count(*) as total from ${TBL_COURSE} 
+        inner join category
+        on course.IdCategory = category.Id
+        where category.Id in (select category.Id
+            from headercategory
+            inner join category
+            on headercategory.Id = category.HeaderCategoryId
+            where category.HeaderCategoryId = ${domainId})`);
+        return rows[0].total;
+    },
+
+    async pageByDomain(domainId, offset) {
+        return await db.load(`select course.IdCourse, FullName, nameCourse, course.Description,course.title,
+        course.nameCourse, category.NameCategory, course.Price, course.SaleCost, course.createdTime, course.nOViews,
+        count(chapter.IdChapter) as numOfChapters
+        from ${TBL_COURSE} 
+        left join user_profile
+        on course.IdTeacher = user_profile.IdUser
+        inner join category
+        on course.IdCategory = category.Id
+        inner join chapter
+        on course.IdCourse = chapter.idCourse
+        where category.Id in (select category.Id
+            from headercategory
+            inner join category
+            on headercategory.Id = category.HeaderCategoryId
+            where category.HeaderCategoryId = ${domainId})
+        group by course.IdCourse
+        order by course.nOViews DESC limit ${config.pagination.limit} offset ${offset}`);
+    },
+
+    
+
+
 };
--- E-learning/routes/front/course.route.js
+++ E-learning/routes/front/course.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controller/courseByCategory.controller');
-router.get('/bycat/:id', controller.allByCategory);
-
+router.get('/category/:id', controller.allByCategory);
+router.get('/domain/:id', controller.allByDomain);
 module.exports = router;
