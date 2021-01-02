b91409aae5353228b4096938ecbe8f5961b28e84
duonglee2311 <duonglee2311@gmail.com>
2021-01-02 20:51:35 +0700

    fix the link error in the fulltextsearch.hbs folder

--- E-learning/models/course.model.js
+++ E-learning/models/course.model.js
@@ -47,7 +47,12 @@ module.exports = {
         const rows = await db.load(`select course.IdCourse, FullName, nameCourse, course.Description,
         course.nameCourse, category.NameCategory, course.Price, course.SaleCost,course.title,
         course.createdTime, course.nOViews,course.avgRate ,course.subscribe
-        inner join headercategory
+        from ${TBL_COURSE} 
+        left join user_profile
+        on course.IdTeacher = user_profile.IdUser
+        inner join category
+        on course.IdCategory = category.Id
+        left join headercategory
         on category.headercategoryid=headercategory.id
         where course.IdCourse = ${condition}
         group by course.idcourse`);
