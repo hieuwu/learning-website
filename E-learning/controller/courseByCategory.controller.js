e760099b7f794acf3b69de877558a1b11fd5487b
hieuvu99 <star.hieu.99@gmail.com>
2021-01-03 14:54:53 +0700

    View couse detail by clicking into item in category list

--- E-learning/controller/courseByCategory.controller.js
+++ E-learning/controller/courseByCategory.controller.js
@@ -1,18 +1,21 @@
-const courseModel = require('../models/course.model');
-const config = require('../config/default.json');
+const courseModel = require("../models/course.model");
+const config = require("../config/default.json");
 module.exports = {
-  allByCategory: async  (req, res) => {
+  allByCategory: async (req, res) => {
     let page = +req.query.page || 1;
     if (page == 0) page = 1;
     let offset = (page - 1) * config.pagination.limit;
-    let listOfCourses = await courseModel.pageBycat(req.params.categoryId ,offset);
+    let listOfCourses = await courseModel.pageBycat(
+      req.params.categoryId,
+      offset
+    );
     const total = await courseModel.countByCat(req.params.categoryId);
-    let nPages = Math.ceil(total/config.pagination.limit);
+    let nPages = Math.ceil(total / config.pagination.limit);
     let page_items = [];
-    for(i = 1; i <= nPages; i++) {
+    for (i = 1; i <= nPages; i++) {
       const item = {
-        value:i
-      }
+        value: i,
+      };
       page_items.push(item);
     }
 
@@ -23,21 +26,24 @@ module.exports = {
       can_go_prev: page > 1,
       prev_value: page - 1,
       next_value: page + 1,
+      title: listOfCourses[0].NameCategory,
     });
   },
 
-  allByDomain: async  (req, res) => {
+  allByDomain: async (req, res) => {
     let page = +req.query.page || 1;
     if (page == 0) page = 1;
     let offset = (page - 1) * config.pagination.limit;
-    let listOfCourses = await courseModel.pageByDomain(req.params.id ,offset);
+    let title = await courseModel.getDomainName(req.params.id);
+    console.log("title:", title[0].HeaderNameCategory);
+    let listOfCourses = await courseModel.pageByDomain(req.params.id, offset);
     const total = await courseModel.countByDomain(req.params.id);
-    let nPages = Math.ceil(total/config.pagination.limit);
+    let nPages = Math.ceil(total / config.pagination.limit);
     let page_items = [];
-    for(i = 1; i <= nPages; i++) {
+    for (i = 1; i <= nPages; i++) {
       const item = {
-        value:i
-      }
+        value: i,
+      };
       page_items.push(item);
     }
 
@@ -48,6 +54,7 @@ module.exports = {
       can_go_prev: page > 1,
       prev_value: page - 1,
       next_value: page + 1,
+      title:  title[0].HeaderNameCategory,
     });
   },
 };
