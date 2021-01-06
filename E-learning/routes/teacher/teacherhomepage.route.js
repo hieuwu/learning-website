9228ee827bafbdd71109d668426f590b025a418a
minhhien21 <56583941+minhhien21@users.noreply.github.com>
2021-01-06 17:47:49 +0700

    - delete config

--- E-learning/routes/teacher/teacherhomepage.route.js
+++ E-learning/routes/teacher/teacherhomepage.route.js
@@ -2,10 +2,4 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controller/teacher/homepage.controller');
 router.get('/', controller.getHomePage);
-router.get("/", (req, res) => {
-    res.render('teacher/teacherhomepage', {
-        layou: 'signup',
-        style: 'signup'
-    });
-})
 module.exports = router;
