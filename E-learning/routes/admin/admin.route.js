7ecc696f3a2013e0016eb69155479fb7c07bd575
hieuvu99 <star.hieu.99@gmail.com>
2021-01-07 13:21:31 +0700

    add route to edit category page

--- E-learning/routes/admin/admin.route.js
+++ E-learning/routes/admin/admin.route.js
@@ -8,5 +8,6 @@ router.get('/course/detail/:id', controller.getCourseDetail);
 router.get('/course/edit/:id', controller.getEditCoursePage);
 router.post('/course/edit/:id', controller.editCourse);
 router.get('/category/all', controller.getCategoryPage);
+router.get('/category/edit/:id', controller.getEditCategoryPage);
 
 module.exports = router;
